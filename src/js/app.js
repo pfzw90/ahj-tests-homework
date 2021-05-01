6dfd643a4b857037df3f74b895b29466f6562c06
Natalya Chekhonina <pfzw90@gmail.com>
2021-05-01 21:22:10 +0300

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,6 @@
-// @license Все картинки спи...взяты с сайта https://findicons.com/ и распространяются по бесплатной лицензии. А некоторые я вообще сама сделала :)))
-
+/*
+@license Все картинки спи...взяты с сайта https://findicons.com/ и распространяются по бесплатной лицензии. А некоторые я вообще сама сделала :)))
+*/
 import CardChecker from './cardChecker';
 
 const cardChecker = new CardChecker();
